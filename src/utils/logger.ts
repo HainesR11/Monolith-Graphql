3ac9495ff298dc74acc3edac66cefc52f8b6ee14
Rhys Haines <rhys.haines11@gmail.com>
2025-05-07 12:06:33 +0100

    environment added to logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,9 @@
-import {
-  add,
-  addColors,
-  createLogger,
-  error,
-  format,
-  info,
-  transports,
-} from "winston";
-const { combine, prettyPrint, label, colorize, errors } = format;
-import { TransformableInfo } from "logform";
+import { createLogger, format, transports } from "winston";
+const { combine, prettyPrint, errors } = format;
 import { TLogger } from "../types/types";
+import { TransformableInfo } from "logform";
 
-interface TTransportInfo extends TransformableInfo {
+interface ITransformableInfo extends TransformableInfo {
   query?:
     | {
         type: "Query" | "Mutation" | "Not used";
@@ -21,7 +13,7 @@ interface TTransportInfo extends TransformableInfo {
     | undefined;
 }
 
-const messageSwitch = ({ type, query, level, message }: TTransportInfo) => {
+const messageSwitch = ({ type, query, level, message }: ITransformableInfo) => {
   switch (true) {
     case type === "Query" || type === "Mutation":
       return `${type} ran Successfully`;
@@ -32,18 +24,25 @@ const messageSwitch = ({ type, query, level, message }: TTransportInfo) => {
   }
 };
 
-const querySwitch = ({ query }: TTransportInfo) => {
+const querySwitch = ({ query }: ITransformableInfo) => {
   switch (true) {
     case query?.type === "Query" || query?.type === "Mutation":
       return { query: query?.query, type: query?.type };
   }
 };
 
-const myFormatter = format((info: TTransportInfo) => {
+const environment = () => {
+  if (process.env.NODE_ENV === "prod") {
+    return "production";
+  }
+  return "development";
+};
+
+const customFormatter = format((info: ITransformableInfo) => {
   return {
     ...{ ...info, message: messageSwitch(info) },
     ...(info.query !== undefined && querySwitch(info)),
-    environment: process.env.NODE_ENV?.toLocaleUpperCase(),
+    environment: environment().toLocaleUpperCase(),
     timestamp: new Date(),
   };
 })();
@@ -51,7 +50,7 @@ const myFormatter = format((info: TTransportInfo) => {
 export const logger: TLogger = createLogger({
   transports: new transports.Console(),
   format: combine(
-    myFormatter,
+    customFormatter,
     errors({ stack: true }),
     format.timestamp({ format: "HH:mm:ss YYYY-MM-DD", alias: "timestamp" }),
     prettyPrint()
